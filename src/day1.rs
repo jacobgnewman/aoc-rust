3d7b3c0ed3cd14e2464bc98ca62540082b5d610f
jacobgnewman <57055451+jacobgnewman@users.noreply.github.com>
2024-11-30 22:09:58 -0800

    d1, got to get that time down p2

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,38 @@
+use std::iter::zip;
+use std::num;
+use itertools::Itertools;
 //#[aoc(day1, part1, Chars)]
-pub fn part1(input: &str) -> usize {
-    0
+pub fn part1(input: &str) -> i32 {
+    let mut va = Vec::new();
+    let mut vb = Vec::new();
+
+    for l in input.lines() { 
+        va.push(l[0..5].parse::<i32>().unwrap());
+        vb.push(l[8..].parse::<i32>().unwrap());
+    }
+    va.sort();
+    vb.sort();
+    zip(va.iter(), vb.iter()).map(|(a, b)| (a-b).abs()).sum()
 }
 
-pub fn part2(input: &str) -> usize {
-    1
+pub fn part2(input: &str) -> i32 {
+    let s = input.lines().count();
+    let mut va = Vec::with_capacity(s);
+    let mut vb = Vec::with_capacity(s);
+
+    for l in input.lines() { 
+        va.push(l[0..5].parse::<i32>().unwrap());
+        vb.push(l[8..].parse::<i32>().unwrap());
+    }
+
+    let c  = vb.iter().counts();
+    va.iter()
+      .map(|x| {
+        match c.get(x) {
+                Some(c) => x * (*c as i32),
+                None => 0
+        }})
+      .sum()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate aoc_rust;
+
 use std::fs::File;
 use std::io::{self, Read};
 
@@ -9,13 +11,20 @@ fn read_file(filename: &str) -> io::Result<String> {
 }
 
 use aoc_rust::day1::{part1, part2};
+use std::time::Instant;
 
 fn main() {
     let inp = read_file("input/2024/day1.txt");
     match inp {
         Ok(val) => {
+            let now = Instant::now();
             let p1 = part1(&val);
+            let t1 = now.elapsed();
+            println!("p1 time {:?}", t1);
+            let now = Instant::now();
             let p2 = part2(&val);
+            let t2 = now.elapsed();
+            println!("p2 time {:?}", t2);
             println!("p1: {}", p1);
             println!("p2: {}", p2);
         }
